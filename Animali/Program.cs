c91a368e23607f0c472e1b90292b558ede2bcd21
vcolluto <86895240+vcolluto@users.noreply.github.com>
2023-04-13 11:38:28 +0200

    Esempi late ed early binding

--- Animali/Program.cs
+++ Animali/Program.cs
@@ -1,8 +1,8 @@
 ﻿using Animali;
 
-Animale a1, a2;     
+Animale a1, a2;         //non so ancora cosa ci sarà in a1 e in a2
 
-a1 = new Mucca();       //late binding
+a1 = new Mucca();       //late binding (so che una mucca in fase dopo la chiarazione)
 
 Console.WriteLine("Mucca: ");
 a1.faiVerso(2);       //si sa cosa fare
@@ -13,6 +13,9 @@ Console.WriteLine("\nGatto: ");
 a2.faiVerso(3);       //sarà diverso da quello della mucca!
 a2.dormi();
 
+
+Gatto g;        //Early binding (E' un'istanza di tipo Gatto)
+g= new Gatto();
 
 List<Animale> elencoAnimali = new List<Animale>(); 
 
